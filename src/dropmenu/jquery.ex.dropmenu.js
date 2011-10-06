2e50b967bbcf3a20ebd49feaadd2a53092b755c5
gilpederiva <gilpederiva@gmail.com>
2011-10-05 23:05:52 -0400

    implementando funcionalidades de hover via javascript

--- src/dropmenu/jquery.ex.dropmenu.js
+++ src/dropmenu/jquery.ex.dropmenu.js
@@ -71,84 +71,74 @@
 
                 if ( $elemParent.hasClass("haschild") && !$elemParent.find("a.dropmenu-ex-subitem-active").is("a") ){                    
                     $elemParent.find("ul").eq(0).hide();
-                }                                
-            });               
+                }  
+                
+                
+                //console.log(  );
+                
+                if ( !$elemParent.closest("ul li.primary").find("li a").hasClass("dropmenu-ex-subitem-active")){
+                    
+                    $elemParent.closest("ul li.primary").find("ul").hide();
+                    
+                    
+                }
+                
+            });
+            
+            
+            
+            self.element.find("li").hover(function(){
+                
+                    var $elemParent = $(this);
+
+                if ( $elemParent.hasClass("primary") ){
+                    $(this).addClass("dropmenu-ex-active dropmenu-ex-open");       
+                }else{
+                    $(this).addClass("dropmenu-ex-subitem-active");       
+                }
+
+                if ( $elemParent.hasClass("haschild") ){
+                    $elemParent.find("ul").eq(0).show();
+                }
+                
+                
+            }, function(){
+                
+                
+                var $elemParent = $(this);
+
+                if ( $elemParent.hasClass("primary") ){
+
+                    if ( !$elemParent.find("a.dropmenu-ex-subitem-active").is("a")  )                    
+                        $(this).removeClass("dropmenu-ex-active dropmenu-ex-open");   
 
 
+                }else{
+                    $(this).removeClass("dropmenu-ex-subitem-active");       
+                }
+
+                if ( $elemParent.hasClass("haschild") && !$elemParent.find("a.dropmenu-ex-subitem-active").is("a") ){                    
+                    $elemParent.find("ul").eq(0).hide();
+                }  
+                                
+                
+                
+            });  
+            
+            
+       
+            
+            //adiciona as funcionalidades de navegação pelas teclas: esquerda, direita, cima e baixo
             self.element.find( "li a" ).bind( "keydown.dropmenu", self.keydown );
 
-        /*
-
-        self.element.find("li a").bind("keydown", function(event){
-
-            //seta para direita
-            if (event.keyCode == 39)   {  
-
-                if ( $(this).closest("li").hasClass("primary") ){
-                    $(this).closest("li").next("li").find("a:eq(0)").focus();
-                }else{
-
-                    if ( $(this).closest("li").hasClass("haschild") ){
-                        $(this).closest("li").find("ul:eq(0)").find("li:eq(0) a:eq(0)").focus();
-                    }                       
-                }
-            }
-
-            //seta para esquerda              
-            if (event.keyCode == 37){
-
-                if ( $(this).closest("li").hasClass("primary") ){
-                    $(this).closest("li").prev("li").find("a:eq(0)").focus();
-                }else{
-
-                    if ( $(this).closest("li").closest("ul").closest("li").hasClass("haschild") && $(this).closest("li").is(":first")  ){
-                        $(this).closest("li").closest("ul").closest("li").find("a:eq(0)").focus();                        
-                    }                        
-                }
-            }
-
-            //seta para baixo
-            if (event.keyCode == 40){
-
-                var $elemNext = $(this).next();
-
-                if ( $elemNext.is("ul") && $elemNext.closest("li").hasClass("primary")  ){
-                    $elemNext.find("li:eq(0) a:eq(0)").focus();
-                }else{                                                    
-                    $(this).closest("li").next("li").find("a:eq(0)").focus(); 
-                }
-
-            }
-
-            //seta para cima
-            if ( event.keyCode == 38 ){
-
-                var $elemUp = $(this).closest("li");
-
-                if ( !$elemUp.prev().is("li") ){                          
-                    $elemUp.closest("ul").closest("li").find("a:eq(0)").focus();
-                }else{                                                    
-                    $(this).closest("li").prev("li").find("a:eq(0)").focus(); 
-                }                                       
-            }
-
-        }).bind("mouseenter", function(event){                              
-            // $(this).focus();                              
-            }).bind("mouseleave", function(event){
-            // $(this).blur(); 
-            });
-
-      */
         },
 
 
         keydown: function( event ){
 
             var keyCode = $.ui.keyCode;
-            var elemParent, elemNext = null;
-            
-            
-
+            var elemParent = null;
+                        
             switch( event.keyCode ){
 
                 case keyCode.RIGHT:
@@ -183,25 +173,30 @@
                     
              
                 case keyCode.UP:
-                    alert("cima");
+                    
+                    var elemUp = $(this).closest("li");
+
+                    if ( !elemUp.prev().is("li") ){                          
+                        elemUp.closest("ul").closest("li").find("a:eq(0)").focus();
+                    }else{                                                    
+                        $(this).closest("li").prev("li").find("a:eq(0)").focus(); 
+                    }      
+                    
                     break;
              
              
                 case keyCode.DOWN:
              
-                    elemNext = $(this).next();
+                    elemParent = $(this).next();
 
-                    if ( elemNext.is("ul") && elemNext.closest("li").hasClass("primary")  ){
-                        elemNext.find("li:eq(0) a:eq(0)").focus();
+                    if ( elemParent.is("ul") && elemParent.closest("li").hasClass("primary")  ){
+                        elemParent.find("li:eq(0) a:eq(0)").focus();
                     }else{                                                    
                         $(this).closest("li").next("li").find("a:eq(0)").focus(); 
                     }
-             
-                 
+                    
                     break;
-
-
-            }
+            }//end switch
 
 
         }
