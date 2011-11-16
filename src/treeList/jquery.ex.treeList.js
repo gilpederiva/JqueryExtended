315a56863e9e502e2fc57b52d60d3c3fa08428a2
gilbertopederiva <gilbertopederiva@ufgd.edu.br>
2011-11-16 10:04:50 -0300

    implementação da lista

--- src/treeList/jquery.ex.treeList.js
+++ src/treeList/jquery.ex.treeList.js
@@ -1,6 +1,16 @@
+/**
+ * Tree List Jquery Plugin
+ * 
+ * @Author Gilberto Pederiva
+ * @version 0.1
+ *
+ *
+ * @Description: Transforma uma Lista HTML em uma estrutura de árvore 
+ *
+**/
+
 (function( $ ) {
-    
-    
+        
     
     var methods = {
                     
@@ -8,13 +18,14 @@
             
             var settings = {
                 'speedEffect' : 200,
-                'script': ''                                
+                'script': '',
+                'persistent': false,
+                'cookiePrefix':'tree_list_'
             }
                         
             var listAncoras = null;
-            
-    
-            
+            var listItems = null;
+               
             return this.each(function(){
         
                 if ( options ) { 
@@ -23,7 +34,8 @@
                 
                 $(this).find("li").addClass("collapsed");
                
-                listAncoras = $(this).find("a");
+                listItems = $(this).find("li");
+                listAncoras = listItems.find("a");
                 
                 listAncoras.live('click', function(){
                     
@@ -32,19 +44,26 @@
                     if ( itemList.hasClass("expanded") ){
                         
                         itemList.find('ul:eq(0)')
-                                .slideUp( settings.speedEffect  ).end()
-                                .removeClass("expanded")
-                                .addClass("collapsed");   
+                        .slideUp( settings.speedEffect  ).end()
+                        .removeClass("expanded")
+                        .addClass("collapsed");
+                        
+                        if (settings.persistent == true)
+                            $.cookie( settings.cookiePrefix + itemList.attr("id"), null);
                         
                     }else{
                         
+                        var itemId = itemList.attr("id");
+                        
+                        
                         if ( itemList.find('ul:eq(0)').length == 0 && settings.script != '' ){
                                                                                                                                            
-                                itemList.addClass("wait");                                                                                
+                            itemList.addClass("wait");                                                                                
+                                                                                       
+                            methods.getRemoteData(settings, itemId); 
                             
-                                var itemId = $(this).attr("id");
-                            
-                                methods.getRemoteData(settings, itemId);   
+                            if (settings.persistent == true)
+                                $.cookie( settings.cookiePrefix + itemId, 'true');
                                                            
                         }else{
                             
@@ -53,21 +72,40 @@
                             .removeClass( "collapsed" )
                             .addClass( "expanded" );
                             
+                            if (settings.persistent == true)
+                                $.cookie( settings.cookiePrefix + itemId, 'true');
+                            
                         }
                         
                     }
                                                                                 
                 });//end live(click)  
-                                                                                                                                                         
+                
+                /**
+                 * Inicializa a lista conforme os dados armazenados no cookie
+                 */                
+                if (settings.persistent == true){                    
+                
+                    listItems.each(function(){
+                   
+                   
+                        if ( $.cookie( settings.cookiePrefix + $(this).attr("id")  ) == 'true' ){
+                        
+                            $(this).find("a:eq(0)").click();                           
+                        
+                        }
+                   
+                    });                 
+                    
+                }
+                                                                                                                                                                         
             });//end each
-        
-            
-            
-            
-        
+                                                    
         },//end init
         
-        
+        /**
+         * Obtem dados remotamente
+         */
         getRemoteData: function(settings, itemId){
             
             $.get( settings.script , {
@@ -86,33 +124,66 @@
             
         },
         
+        /**
+         * Expande todos os itens da lista
+         */
         expandeAllNodes: function(){
             
             return this.each(function(){
                
-               $(this).find("li.collapsed > a").click();
+                $(this).find("li.collapsed > a").click();
                
             });
             
         },
         
-        collapseAllNodes: function(novo){
+        /*
+         * Recolhe todos os itens da lista
+         */
+        collapseAllNodes: function(){
             
           
             return this.each(function(){
                
-               $(this).find("li.expanded > a").click();
+                $(this).find("li.expanded > a").click();
                
             });
             
+        },
+        
+        /**
+         * Expande um item específico da lista
+         */
+        expandeNode: function(nodeId){
+            
+            if ( $(nodeId).length <= 0){
+                
+                alert("Não existe esse Item!");
+                return false;
+                
+            }            
+            
+            var node = $(nodeId);
+            var parent = node.closest("ul");
+                    
+            while(!parent.hasClass("treeList")){
+                
+                node = node.closest("ul").closest("li");
+                
+                if ( !node.hasClass("expanded") ){
+                    node.find("a:eq(0)").click();
+                }
+                
+                parent = node.closest("ul");
+                
+            }            
+                                    
         }
 
     };
     //fim do method
        
-       
-
-    
+          
     //define a inicialização do plugin
     $.fn.treeList = function( method ) {
         if ( methods[method] ) {
@@ -126,4 +197,3 @@
       
     
 })( jQuery );
-
