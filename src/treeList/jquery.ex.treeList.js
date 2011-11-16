e1798da5141e00a4e1b7af67eae80da3634eeadc
gilpederiva <gilpederiva@gmail.com>
2011-11-15 23:32:59 -0300

    adicionado funcionalides no tree_list

--- src/treeList/jquery.ex.treeList.js
+++ src/treeList/jquery.ex.treeList.js
@@ -1,73 +1,117 @@
 (function( $ ) {
     
+    
+    
     var methods = {
-     
+                    
         init : function( options ) {
             
             var settings = {
                 'speedEffect' : 200,
-                'script': '',
-                'anchorClass': '.classe',
-                'parametro':'id' 
+                'script': ''                                
             }
                         
             var listAncoras = null;
-                
+            
+    
+            
             return this.each(function(){
         
                 if ( options ) { 
                     $.extend( settings, options );
                 }
                 
+                $(this).find("li").addClass("collapsed");
                
-                listAncoras = $(this).find("a"+settings.anchorClass);
+                listAncoras = $(this).find("a");
                 
                 listAncoras.live('click', function(){
                     
-                    var listItem = $(this).closest("li");
+                    var itemList = $(this).closest("li");
                     
-                    if ( listItem.hasClass("expanded") ){
+                    if ( itemList.hasClass("expanded") ){
                         
-                        listItem.find('ul:eq(0)').slideUp( settings.speedEffect  ).end().removeClass("expanded").addClass("collapsed");   
+                        itemList.find('ul:eq(0)')
+                                .slideUp( settings.speedEffect  ).end()
+                                .removeClass("expanded")
+                                .addClass("collapsed");   
                         
                     }else{
                         
-                        if ( listItem.find('ul:eq(0)').length == 0 ){
+                        if ( itemList.find('ul:eq(0)').length == 0 && settings.script != '' ){
+                                                                                                                                           
+                                itemList.addClass("wait");                                                                                
                             
-                            listItem.addClass("loading");
+                                var itemId = $(this).attr("id");
                             
-                            var vetor = $(this).attr("id").split("-");
-                            var parametro = settings.parametro;
-                            $.get( settings.script , {parametro : vetor[1] },
-                            
-                                function(data){
-                                    
-                                    $(".loading").append(data)
-                                                 .children('ul')
-                                                 .slideDown( settings.speedEffect  ).end()
-                                                 .removeClass("collapsed loading")
-                                                 .addClass("expanded");
-                                    
-                                }, "html");
-                                
+                                methods.getRemoteData(settings, itemId);   
+                                                           
                         }else{
-                            listItem.find( "ul:eq(0)" )
-                                    .slideDown( settings.speedEffect  ).end()
-                                    .removeClass( "collapsed" )
-                                    .addClass( "expanded" );
+                            
+                            itemList.find( "ul:eq(0)" )
+                            .slideDown( settings.speedEffect  ).end()
+                            .removeClass( "collapsed" )
+                            .addClass( "expanded" );
+                            
                         }
                         
                     }
                                                                                 
-                });  
+                });//end live(click)  
                                                                                                                                                          
-            });
+            });//end each
         
-        }        
+            
+            
+            
+        
+        },//end init
+        
+        
+        getRemoteData: function(settings, itemId){
+            
+            $.get( settings.script , {
+                parameter : itemId
+            },
+                            
+            function(data){
+                                    
+                $(".wait").append(data)
+                .children('ul')
+                .slideDown( settings.speedEffect  ).end()
+                .removeClass("collapsed wait")
+                .addClass("expanded");
+                                    
+            }, "html");            
+            
+        },
+        
+        expandeAllNodes: function(){
+            
+            return this.each(function(){
+               
+               $(this).find("li.collapsed > a").click();
+               
+            });
+            
+        },
+        
+        collapseAllNodes: function(novo){
+            
+          
+            return this.each(function(){
+               
+               $(this).find("li.expanded > a").click();
+               
+            });
+            
+        }
+
     };
-    //fim dos métodos
+    //fim do method
        
-    
+       
+
     
     //define a inicialização do plugin
     $.fn.treeList = function( method ) {
@@ -76,9 +120,10 @@
         } else if ( typeof method === 'object' || ! method ) {
             return methods.init.apply( this, arguments );
         } else {
-            $.error( 'Method ' +  method + ' does not exist on jQuery.fieldsetGP' );
+            $.error( 'Method ' +  method + ' does not exist on jQuery.TreeList' );
         }                 
     };
       
     
 })( jQuery );
+
